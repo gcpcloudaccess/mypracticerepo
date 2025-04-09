e70e399041f0ee5c26d9528836d6799a634b85f8
manishsharma141121 <manish.sharma14@wipro.com>
2025-04-09 15:18:40 +0530

    removed unstaged file

--- multiclusterbigtable/modules/multicluster/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "instance_id" {
-  value = google_bigtable_instance.bt-instance.name
-}
-
-output "cluster_ids" {
-  value = [for cluster in google_bigtable_instance.bt-instance : cluster.cluster_id]
-}
