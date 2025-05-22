8ab72ed5347f590541dee24bdc802b26d9b9e4a2
manishsharma141121 <manish.sharma14@wipro.com>
2025-05-22 18:57:55 +0530

    my aws secrets code

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_secretsmanager_secret" "vault-secrets" {
+  name        = var.my-app-secret
+  description = "My application secret"
+}
+
+resource "aws_secretsmanager_secret_version" "my_secret_version" {
+  secret_id     = aws_secretsmanager_secret.vault-secrets.id
+  secret_string = var.secrets_json
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,10 @@
 variable "secrets_json" {
   default = jsonencode(jsondecode(file("${path.module}/vaultsecrets.json")))
 }
+
+
+variable "my-app-secret" {
+  description = "My application secret"
+  type        = string
+  default     = " my-app-secret"
+}
