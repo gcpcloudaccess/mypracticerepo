5463c07e167cc86186552c1420b2ea3e08bc6be2
manishsharma141121 <manish.sharma14@wipro.com>
2025-05-23 14:42:39 +0530

    aws app secrets

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-# variable "secrets_json" {
-#   description = "Secrets loaded from vaultsecrets.json"
-#   type        = any
-# #   default = jsondecode(file("vaultsecrets.json"))
-# }
 
 variable "my-app-secret" {
   description = "My application secret"
