d94decc11dd0da67f5cc1a7648e3cf11b6aa0d04
Manish Sharma <136334155+gcpcloudaccess@users.noreply.github.com>
2025-06-30 20:27:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,3 +17,24 @@ resource "google_compute_subnetwork" "custom_subnet" {
   region        = var.region
   network       = google_compute_network.custom_vpc.id
 }
+
+
+------------------
+Calling module:
+module "my_network" {
+  source       = "./modules/network"
+  network_name = "custom-vpc"
+  subnets = [
+    {
+      name       = "subnet-a"
+      cidr       = "10.10.0.0/24"
+      region     = "us-central1"
+    },
+    {
+      name       = "subnet-b"
+      cidr       = "10.20.0.0/24"
+      region     = "us-west1"
+    }
+  ]
+  project_id   = var.project_id
+}
